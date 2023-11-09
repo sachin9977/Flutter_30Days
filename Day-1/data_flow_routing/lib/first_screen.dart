d3e1cc3c10fe7aaf6ad8ae1f9d97624a08eea229
Sachin Choudhary <sc09626@gmail.com>
2023-11-09 12:03:47 +0530

    1st Task Done

---
+++ Day-1/data_flow_routing/lib/first_screen.dart
@@ -0,0 +1,60 @@
+import 'package:data_flow_routing/second_screen.dart';
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+        "Data Flow Routing",
+        style: TextStyle(color: Colors.white),
+      )),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: TextButton(
+              onPressed: () => _navigate(context),
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.black)),
+              child: const Text(
+                "Go To Second Screen",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// Function for sending data forward & backward
+Future<void> _navigate(BuildContext context) async {
+  // here we are storing the value which we are receiving from second screen (OnPop)
+  final result = await Navigator.push(
+    context,
+// We are sending data to second Screen
+    MaterialPageRoute(
+        builder: (context) => const SecondScreen(
+              s: 'Data Returning Page',
+            )),
+  );
+  // ignore: use_build_context_synchronously
+  ScaffoldMessenger.of(context)
+    ..removeCurrentSnackBar()
+    ..showSnackBar(
+        // Here we are handling null value
+        SnackBar(
+            content: result != null
+                ? Text('$result')
+                : const Text("Not selected any option")));
+}
---
+++ Day-1/data_flow_routing/lib/second_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatefulWidget {
+  // Created constructor function for recevieng data from first screen
+
+  const SecondScreen({super.key, required this.s});
+
+  final String s;
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.s,
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            onPressed: () {
+              // Sending data backward (on First Screen)
+              Navigator.pop(context, 'Yeah!!');
+            },
+            style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.black)),
+            child: const Text(
+              "Return Yes",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          Center(
+            child: TextButton(
+              onPressed: () {
+                // Sending data backward (on First Screen)
+                Navigator.pop(context, 'Nope!!');
+              },
+              style: const ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.black)),
+              child: const Text(
+                "Return No",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
