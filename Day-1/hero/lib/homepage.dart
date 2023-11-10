4d56f1a748774c8c1e131fd4963e95b949506b0a
Sachin Choudhary <sc09626@gmail.com>
2023-11-10 09:40:17 +0530

    hero animation updated

--- Day-1/hero/lib/homepage.dart
+++ Day-1/hero/lib/homepage.dart
@@ -1,38 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:hero/next_screen.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class Item {
+  final String urlImage;
+  final String title;
 
-  @override
-  State<HomePage> createState() => _HomePageState();
+  Item({required this.urlImage, required this.title});
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePage extends StatelessWidget {
+  final items = <Item>[
+    Item(title: 'Item 1', urlImage: 'https://picsum.photos/250?image=9'),
+    Item(
+        title: 'Item 2',
+        urlImage:
+            'https://img.freepik.com/free-photo/puppy-that-is-walking-snow_1340-37228.jpg'),
+    Item(
+        title: 'Item 3',
+        urlImage:
+            'https://t3.ftcdn.net/jpg/05/93/79/50/360_F_593795007_nRQ8Z5M7xdZnT3y7YtOE8ypwexuUb6kP.jpg'),
+  ];
+
+  HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 6,
-        shadowColor: Colors.black,
-        title: const Text("Hero Animation"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: GestureDetector(
-              onTap: () =>
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return const NextScreen();
-              })),
-              child: Hero(
-                  tag: 'Hero Animation',
-                  child: Image.network('https://picsum.photos/250?image=9')),
-            ),
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          elevation: 6,
+          shadowColor: Colors.black,
+          title: const Text("Hero Animation"),
+        ),
+        body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+            return GestureDetector(
+              onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => NextScreen(item: item),
+                  )),
+              child: Row(
+                children: [
+                  Hero(
+                      transitionOnUserGestures: true,
+                      tag: "image1",
+                      child: buildImage(item.urlImage)),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Text(
+                    'Item ${index + 1}',
+                    style: const TextStyle(fontSize: 20),
+                  )
+                ],
+              ),
+            );
+          },
+        ));
   }
 }
+
+Widget buildImage(String urlImage) => Image.network(
+      urlImage,
+      fit: BoxFit.cover,
+      width: 100,
+      height: 100,
+    );
--- Day-1/hero/lib/next_screen.dart
+++ Day-1/hero/lib/next_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:hero/homepage.dart';
 
 class NextScreen extends StatefulWidget {
-  const NextScreen({super.key});
+  final Item item;
+
+  const NextScreen({super.key, required this.item});
 
   @override
   State<NextScreen> createState() => _NextScreenState();
@@ -11,19 +14,19 @@ class _NextScreenState extends State<NextScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: GestureDetector(
-              onTap: () => Navigator.pop(context),
-              child: Hero(
-                  tag: 'Hero Animation',
-                  child: Image.network('https://picsum.photos/250?image=9')),
-            ),
-          )
-        ],
-      ),
-    );
+        body: Hero(
+            transitionOnUserGestures: true,
+            tag: widget.item,
+            child: buildImage()));
   }
+
+  Widget buildImage() => AspectRatio(
+        aspectRatio: 1,
+        child: Image.network(
+          widget.item.urlImage,
+          fit: BoxFit.cover,
+          width: 100,
+          height: 100,
+        ),
+      );
 }
